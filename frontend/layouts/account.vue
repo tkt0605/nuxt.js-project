affa6d04f984de879b3a799dd82f60a995c4ea12
tkt0605 <takato.komada2005@outlook.jp>
2024-11-20 19:00:28 +0900

    各コンテナでの設定を確認

---
+++ frontend/layouts/account.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <!-- アカウント作成ページ専用のデザイン -->
+      <main>
+        <nuxt />
+      </main>
+    </div>
+  </template>
+  
+  <style>
+  main {
+    background-color: #f9f9f9;
+    padding: 2rem;
+  }
+  </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <app/>
   <div>Hello World</div>
+  <nuxt-link to="/about">About</nuxt-link>
 </template>
 <style>
 *{
@@ -18,6 +18,7 @@ aside{
 </style>
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  layout: 'default',
 }
 </script>
