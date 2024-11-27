fb21d6c7b05798bc5b4c79ba55b28983d11e329a
tkt0605 <takato.komada2005@outlook.jp>
2024-11-27 22:15:31 +0900

    ＨＴＭＬページの基本的デザイン

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,21 +1,33 @@
 <template>
     <div>
         <div class="header">
-            <div class="headerline">
+            <div class="headerline"> 
                 <div class="container">
-                    <a class="logo">
-                        ToDO<small class="logo_small"> By Typewriter</small>
-                    </a>
+                    <a class="logo">ToDO<small class="logo_small"> By Typewriter</small></a>
+                    <div class="account_form">
+                        <button class="login">ログイン</button>
+                        <button class="signup">サインアップ</button>
+                    </div>
                 </div>
             </div>
         </div>
         <aside>
             <ul>
-                <li class="create_todo">New Todo</li>
-                <li class="log_todo">2024/11/25-NewToDO</li>
-                <li class="log_todo">2024/11/25-NewToDO</li>
-                <li class="log_todo">2024/11/25-NewToDO</li>
-                <li class="log_todo">2024/11/25-NewToDO</li>
+                <li class="create_todo">
+                    <NuxtLink class="new_todo" to="/">New ToDO</NuxtLink>
+                </li>
+                <li class="log_todo">
+                    <NuxtLink class="todo_tab" to="/">2024/11/26 ToDO 1</NuxtLink>
+                </li>
+                <li class="log_todo">
+                    <NuxtLink class="todo_tab" to="/">2024/11/26 ToDO 1</NuxtLink>
+                </li>
+                <li class="log_todo">
+                    <NuxtLink class="todo_tab" to="/">2024/11/26 ToDO 1</NuxtLink>
+                </li>
+                <li class="log_todo">
+                    <NuxtLink class="todo_tab" to="/">2024/11/26 ToDO 1</NuxtLink>
+                </li>
             </ul>
         </aside>
     </div>
@@ -36,16 +48,21 @@ aside{
     max-width: 255px;
     width: 100%;
     height: 100%;
+    margin: 0;
+    position: fixed;
+    border-right: 1px solid #ccc;
 }
 .headerline{
     padding-left: 20px;
     padding-right: 20px;
 }
 .container{
-    padding-right: 15px;
-    padding-left: 15px;
+    padding-right: 20px;
+    padding-left: 20px;
     margin-right: auto;
     margin-left: auto;
+    display: flex;
+    gap: 450px;
 }
 .logo{
     font-size: 20px;
@@ -54,20 +71,72 @@ aside{
     align-items: center;
 }
 .header{
-    background: rgb(230, 230, 230);
-    /* padding: 1.5rem; */
+    background: rgb(226, 226, 226);
+    padding: 1.0rem;
     position: fixed;
-    width: 100%;
+    top: 0;
+    left: 255px; 
+    width: calc(100% - 255px); 
+    z-index: 10;
 }
 ul{
-    padding: 30px;
+    padding: 30px 20px;
+    padding-left: 0;
+    font-weight: bold;
+}
+li{
+    margin-bottom: 20px;
+    margin: 20px 20px 20px 20px;
+    list-style: none;
 }
 li .create_todo{
-    padding: 15px;
     font-size: 15px;
+    align-items: center;
+    margin-left: 15px;
+}
+.new_todo{
+    padding: 10px 10px;
+    border-radius: 5px;
+    color: black;
+    text-decoration: none;
+}
+.new_todo:hover{
+    background-color: #ccc
+}
+.todo_tab{
+    padding: 10px 10px;
+    border-radius: 5px;
+    color: black;
+    text-decoration: none;
+}
+.todo_tab:hover{
+    background-color: #ccc;
 }
 li .log_todo{
     padding: 10px;
     margin: 10px;
+    list-style: none;
+}
+.login{
+    background-color: black;
+    padding: 10px;
+    margin: 10px;
+    color: white;
+    border-radius: 20px;
+    border: 1px solid rgb(226, 226, 226);
+    width: 100px;
+}
+.signup{
+    padding: 10px;
+    margin: -10px;
+    border-radius: 20px;
+    border: 1px solid rgb(226, 226, 226);
+    width: 120px;
+}
+.account_form{
+    left: 150px;
+    top: 0;
+    margin-left: 20px;
+    gap: 15px;
 }
 </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <nav class="bord">
-    <ul class="todo_border">
+    <ul class="todo_list">   
+      <li class="item">
+        <input type="checkbox" class="checkboxs"/>
+        <div class="time">time</div>
+        <div class="text">Title</div>
+      </li>      
     </ul>
   </nav>
 </template>
@@ -14,6 +19,77 @@ export default {
 </script>
 <style>
 .bord{
-  
+  background: rgb(226, 226, 226);
+  padding: 1.0rem;
+  position: fixed;
+  top: 100px;
+  left: 450px; 
+  width: calc(100% - 450px); 
+  z-index: 0;
+}
+.todo_list{
+  background-color: white;
+  /* padding: 20px; */
+  border-radius: 10px;
+  border: 1px solid #ccc;
+  box-shadow: inset;
+  max-width: 680px;
+  max-height: 350px;
+  width: 100%;
+  height: 100%;
+}
+/* Individual items */
+.item {
+  display: flex;
+  align-items: center;
+  padding: 15px 20px;
+  border-bottom: 1px solid #eceff1;
+  transition: background-color 0.3s ease;
+}
+
+.item:last-child {
+  border-bottom: none;
+}
+
+/* Hover effect for items */
+.item:hover {
+  background-color: #f1f5f9;
+}
+
+/* Checkbox styling */
+.checkboxs {
+  margin-right: 15px;
+  width: 20px;
+  height: 20px;
+  accent-color: #007bff; /* Modern browsers */
+  cursor: pointer;
+}
+
+/* Time styling */
+.time {
+  font-size: 14px;
+  color: #6c757d;
+  margin-right: 20px;
+  flex-shrink: 0;
+}
+
+/* Text styling */
+.text {
+  font-size: 16px;
+  font-weight: 500;
+  color: #333;
+  flex-grow: 1;
+  word-wrap: break-word;
+}
+
+/* Completed task (when checkbox is checked) */
+.checkboxs:checked + .time + .text {
+  text-decoration: line-through;
+  color: #6c757d;
+}
+
+/* Add some padding around the list */
+.todo_list {
+  padding: 10px 0;
 }
 </style>
